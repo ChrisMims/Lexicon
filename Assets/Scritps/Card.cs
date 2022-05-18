d7c37cce522fc5105136d0dc5eedbe66958fc79a
Thingula <thingula@gmail.com>
2022-05-18 14:45:50 -0400

    small changes

--- Assets/Scritps/Card.cs
+++ Assets/Scritps/Card.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public bool isPlayed;
+    public int handIndex;
+    public int matIndex;
+    GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
