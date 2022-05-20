b208b47ea2244b208b99b7bfb7a3860a5eaec77e
Thingula <thingula@gmail.com>
2022-05-20 13:08:21 -0400

    added to the game board

--- Assets/Scritps/Card.cs
+++ Assets/Scritps/Card.cs
@@ -8,15 +8,23 @@ public class Card : MonoBehaviour
     public int handIndex;
     public int matIndex;
     GameManager gm;
+
+    public int cardId;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void MoveToDiscardPile()
+    {
+        gm.playerDiscardPile.Add(this);
+        gameObject.SetActive(false);
     }
 }
